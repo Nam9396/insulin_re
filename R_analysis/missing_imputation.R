500a6b57faf18ac2851ed6b9ea5e080e4512d6e6
Nam9396 <nguyenthanhnam9396@gmail.com>
2025-10-20 23:11:44 +0700

    first commit

---
+++ R_analysis/missing_imputation.R
@@ -0,0 +1,27 @@
+# Nạp toàn bộ các gói cần thiết
+library(tidyverse)   # xử lý, trực quan hóa dữ liệu
+library(readxl)      # đọc file Excel (.xlsx)
+library(openxlsx)    # ghi/xuất Excel
+library(psych)       # thống kê mô tả, kiểm định tâm lý
+library(car)         # hồi quy, kiểm định mở rộng
+library(mice)        # bù dữ liệu thiếu
+library(naniar)      # kiểm tra & trực quan hóa dữ liệu thiếu
+library(writexl)     # một package khác để xuất file excel
+
+df <- read_excel("data/risk_factors.xlsx")
+head(df)
+
+glimpse(df)
+str(df)
+miss_var_summary(df)
+
+pool_df = mice(df, seed = 1234, printFlag = F)
+pool_df$imp$gestational_weight_gain
+
+imputed_df <- complete(pool_df, action=4)
+glimpse(imputed_df)
+View(miss_var_summary(imputed_df))
+
+# write.csv(imputed_df, "imputed_data.csv", row.names = FALSE)
+write_xlsx(imputed_df, "imputed_data.xlsx")
+
