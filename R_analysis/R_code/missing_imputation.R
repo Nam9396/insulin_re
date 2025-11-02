1cd6d6418cc3d08c6997a093559f430bfda8a558
Nam9396 <nguyenthanhnam9396@gmail.com>
2025-11-02 13:40:29 +0700

    total_exwas

--- R_analysis/R_code/missing_imputation.R
+++ R_analysis/R_code/missing_imputation.R
@@ -19,7 +19,7 @@ View(miss_var_summary(df))
 
 pool_df = mice(df, seed = 1234, printFlag = F)
 pool_df$imp$gestational_weight_gain
-
+a
 imputed_df <- complete(pool_df, action=1)
 glimpse(imputed_df)
 View(miss_var_summary(imputed_df))
@@ -27,5 +27,12 @@ View(miss_var_summary(imputed_df))
 # không thể nào impute được các biến: CES_D, PSS, RSE
 
 # write.csv(imputed_df, "imputed_data.csv", row.names = FALSE)
-write_xlsx(imputed_df, "../data/imputed_data.xlsx")
+# write_xlsx(imputed_df, "../data/imputed_data.xlsx")
 
+### thử cách khác 
+
+df_imp <- mice(
+  df,
+  pred = quickpred(df, mincor = 0.2, minpuc = 0.4),
+  seed = 38788, m = 10, maxit = 10, printFlag = FALSE
+)
